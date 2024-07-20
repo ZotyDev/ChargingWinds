cb70d087460a484ed6ea7fb90c45a404dce6afbe
Zoty <leonardokronbauer2002@gmail.com>
2024-07-20 20:23:10 -0300

    Initial commit

---
+++ src/main/java/dev/zoty/chargingWinds/ChargingWinds.java
@@ -0,0 +1,19 @@
+package dev.zoty.chargingWinds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChargingWinds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        getLogger().info("Hello!!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
